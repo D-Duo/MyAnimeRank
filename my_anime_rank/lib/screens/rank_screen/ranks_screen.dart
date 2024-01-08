f2a785a075dd4a9f9f4d13ae1dec6abef2b5f934
Luis González <lluis.gora@gmail.com>
2024-01-08 17:54:51 +0100

    Change of name and Logo in android
    -Cambio de nombre y imagen de la app de andriod
    -Añadido 2 diseños (uno con fondo y otro sin) a la carpeta assets
    -Cambiado el Text("MAR"...) por la nueva imagen sin fondo de assets

--- my_anime_rank/lib/screens/rank_screen/ranks_screen.dart
+++ my_anime_rank/lib/screens/rank_screen/ranks_screen.dart
@@ -75,13 +75,8 @@ class _RanksScreenState extends State<RanksScreen> {
           scrolledUnderElevation: 0,
           backgroundColor: const Color.fromARGB(255, 19, 28, 39),
           centerTitle: true,
-          title: const Text(
-            "MAR",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          title: Image.asset("assets/MyAnimeRank_Logo_emptyBackground.png",
+              width: 80),
           leading: Padding(
             padding: const EdgeInsets.only(left: 20.0),
             child: GestureDetector(
